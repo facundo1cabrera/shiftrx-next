f7dbae258e5e85fa60e87f3a9c89d9b040045065
facundo1cabrera <cabrera.facundo990@gmail.com>
2024-07-08 04:53:13 +0200

    patch: remove search bar from navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,21 +6,6 @@ import { FaSearch } from "react-icons/fa";
 import { Drawer } from "./Drawer";
 
 export const Navbar = () => {
-    const [search, setSearch] = useState("");
-
-    const handleKeyDown = async (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key !== 'Enter') return;
-    }
-
-    const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
-        event.preventDefault();
-
-        setSearch(event.currentTarget.value)
-    }
-
-
-
-
     return (
         <header className="flex flex-col sm:flex-row items-center justify-between sm:h-16 px-2 sm:px-4 border-b md:px-6">
             <nav className="flex flex-col py-4 sm:py-0 sm:flex-row items-center gap-6">
@@ -35,19 +20,6 @@ export const Navbar = () => {
                 </Link>
             </nav>
             <div className="flex items-center">
-                <div className="flex-1 ml-auto sm:flex-initial pr-4">
-                    <div className="relative">
-                        <FaSearch className={`${search.length > 0 ? 'hidden' : ''} absolute
-                            top-1/2 left-4 transform -translate-y-1/2 h-6 w-6 text-gray-500 dark:text-gray-400`} />
-                        <input
-                            placeholder="Search auction..."
-                            className={` ${search.length > 0 ? 'pl-4' : 'pl-14'} border py-3 sm:w-[300px] md:w-[200px] lg:w-[300px]`}
-                            onKeyDown={handleKeyDown}
-                            onChange={handleSearch}
-                            value={search}
-                        />
-                    </div>
-                </div>
                 <Drawer />
             </div>
         </header>
